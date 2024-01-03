958c2135f3b47ad718196c116ade13d703585378
thenextschool-ai <74172494+thenextschool-ai@users.noreply.github.com>
2024-01-03 15:14:53 +0500

    Dark mode on whole app with switch

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,17 +31,17 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div className='container my-3'>
+    <div className='container my-3' style={{color: props.mode === 'dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} placeholder='Enter Text' onChange={handleonchange} id="exampleFormControlTextarea1" rows="8"></textarea>
+        <textarea style={{backgroundColor : props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'#042743'}} className="form-control" value={text} placeholder='Enter Text' onChange={handleonchange} id="exampleFormControlTextarea1" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleupclick}>Move to upper case</button>
         <button className="btn btn-primary mx-2" onClick={handleloclick}>Move to lower case</button>
         <button className="btn btn-primary mx-2" onClick={handleclearclick}>Click to clear text</button>
         <button className="btn btn-success mx-2" onClick={handlespeakclick}>Click to speak</button>
     </div>
-    <div className="conatiner">
+    <div className="conatiner" style={{color: props.mode === 'dark'?'white':'#042743'}}>
         <h2>Your Text Summary</h2>
         <p><b>{text.split(" ").length}</b> words and <b>{text.length}</b> characters</p>
         <p><b>{ 0.008 * text.split(" ").length}</b> minutes to read</p>
