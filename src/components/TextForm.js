59163a196441030e50e945cba59f7c446d25797e
thenextschool-ai <74172494+thenextschool-ai@users.noreply.github.com>
2024-01-08 10:20:44 +0500

    Some remaining issues has been resolved

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,9 +21,7 @@ export default function TextForm(props) {
   }
 
   const handlecopyclick = () =>{
-    var text = document.getElementById("Box");
-    text.select();
-    navigator.clipboard.writeText(text.value);
+    navigator.clipboard.writeText(text);
     props.showalert("Copied to Clipboard","success");
   }
 
@@ -44,20 +42,20 @@ export default function TextForm(props) {
     <div className='container my-3' style={{color: props.mode === 'dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea style={{backgroundColor : props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'#042743'}} className="form-control" value={text} placeholder='Enter Text' onChange={handleonchange} id="Box" rows="8"></textarea>
+        <textarea style={{backgroundColor : props.mode === 'dark'?'#13466e':'white', color: props.mode === 'dark'?'white':'#042743'}} className="form-control" value={text} placeholder='Enter Text' onChange={handleonchange} id="Box" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleupclick}>Move to upper case</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleloclick}>Move to lower case</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handleclearclick}>Click to clear text</button>
-        <button className="btn btn-primary mx-2 my-2" onClick={handlecopyclick}>Click to copy text</button>
-        <button className="btn btn-success mx-2 my-2" onClick={handlespeakclick}>Click to speak</button>
+        <button disabled = {text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleupclick}>Move to upper case</button>
+        <button disabled = {text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleloclick}>Move to lower case</button>
+        <button disabled = {text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleclearclick}>Click to clear text</button>
+        <button disabled = {text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handlecopyclick}>Click to copy text</button>
+        <button disabled = {text.length === 0} className="btn btn-success mx-2 my-1" onClick={handlespeakclick}>Click to speak</button>
     </div>
     <div className="conatiner" style={{color: props.mode === 'dark'?'white':'#042743'}}>
         <h2>Your Text Summary</h2>
-        <p><b>{text.trim() === '' ? 0 : text.split(" ").length}</b> words and <b>{text.length}</b> characters</p>
-        <p><b>{ 0.008 * text.split(" ").length}</b> minutes to read</p>
+        <p><b>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</b> words and <b>{text.length}</b> characters</p>
+        <p><b>{ 0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length}</b> minutes to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : 'Nothing to preview!!'}</p>
     </div>
     </>
   )
